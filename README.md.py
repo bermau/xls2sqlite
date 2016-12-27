9d28ff1698c315db92c70aedf653950a1a6840f4
ptidr <infobertrand@free.fr>
2016-12-27 16:30:48 +0100

    add README.md

---
+++ README.md.py
@@ -0,0 +1,17 @@
+xlsx2sqlite
+
+A python script to convert xlsx file to sqlite database.
+
+Needs :
+    - python 3
+    - openpyxl
+
+
+potential bug :
+1) problem) 
+    import openpyxl
+ImportError: No module named 'openpyxl'
+
+1) solution) : 
+On windows : 
+    pip3.exe install openpyxl
